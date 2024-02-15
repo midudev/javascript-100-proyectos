b26c9681dc7e3d0ac72408ebe9468348c52baac0
Miguel Ángel Durán <miduga@gmail.com>
2024-02-15 11:51:26 +0100

    Initialize Astro project

---
+++ web/tailwind.config.mjs
@@ -0,0 +1,10 @@
+import animations from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [animations],
+}
