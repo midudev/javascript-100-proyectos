faac0f6e81fe2160ad589c22ddc32863e0e690b5
Miguel Ángel Durán <miduga@gmail.com>
2024-02-15 11:51:55 +0100

    Add script to copy projects to public folder

---
+++ web/scripts/copy-projects.js
@@ -0,0 +1,12 @@
+import fs from 'node:fs/promises'
+
+const files = await fs.readdir('../')
+
+const projects = files.filter(file => /^[0-9]/.test(file))
+
+await Promise.all(
+  projects.map(
+    project => fs.cp(`../${project}`, `public/${project}`, { recursive: true })
+  )
+)
+
