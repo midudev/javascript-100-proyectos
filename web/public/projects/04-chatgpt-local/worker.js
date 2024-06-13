6f9d048a54eb4abe311959e535e9e9f2aa40bb12
Facug03 <facundogonzales1550@gmail.com>
2024-06-13 12:32:03 -0300

    use double quotes

--- web/public/projects/04-chatgpt-local/worker.js
+++ web/public/projects/04-chatgpt-local/worker.js
@@ -1,4 +1,4 @@
-import { WebWorkerMLCEngineHandler } from 'https://esm.run/@mlc-ai/web-llm'
+import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm"
 
 const handler = new WebWorkerMLCEngineHandler()
 
