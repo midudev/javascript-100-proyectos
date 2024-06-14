b2f23043d962b602a0b964e0557e8eb502cc9037
Miguel Ángel Durán <miduga@gmail.com>
2024-06-14 19:03:20 +0200

    Reversed sorting and new code

--- web/public/projects/04-chatgpt-local/worker.js
+++ web/public/projects/04-chatgpt-local/worker.js
@@ -1,4 +1,4 @@
-import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm"
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
 
 const handler = new WebWorkerMLCEngineHandler()
 
